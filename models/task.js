9bf3c286b3aace0b1f7e4905a13e361e13309832
Asmita-000 <163384304+Asmita-000@users.noreply.github.com>
2024-03-14 18:17:32 +0530

    n4

--- models/task.js
+++ models/task.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-  taskName: {
+const productSchema = new mongoose.Schema({
+  productName: {
     type: String,
     trim: true,
+    required: true // Ensuring productName is required for a product
   },
-  createDate: Date,
-  completedDate: Date,
-  completed: Boolean
+  description: {
+    type: String,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true // Ensuring price is required for a product
+  },
+  createDate: {
+    type: Date,
+    default: Date.now // Setting a default value to the current date for createDate
+  },
+  // Assuming there might be additional fields specific to a product, you can add them here
 });
 
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model('Product', productSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 var express = require('express');
-var Task = require('../models/task');
+var Product = require('../models/product'); // Import the Product model
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Task.find()
-    .then((tasks) => {      
-      const currentTasks = tasks.filter(task => !task.completed);
-      const completedTasks = tasks.filter(task => task.completed === true);
+  Product.find()
+    .then((products) => {      
+      // Assuming completed field doesn't exist for products
+      const currentProducts = products;
 
-      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
-      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
+      console.log(`Total products: ${products.length}`);
+      res.render('index', { currentProducts: currentProducts });
     })
     .catch((err) => {
       console.log(err);
@@ -20,19 +20,19 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/addTask', function(req, res, next) {
-  const taskName = req.body.taskName;
+router.post('/addProduct', function(req, res, next) {
+  const productName = req.body.productName;
   const createDate = Date.now();
   
-  var task = new Task({
-    taskName: taskName,
+  var product = new Product({
+    productName: productName,
     createDate: createDate
   });
-  console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
+  console.log(`Adding a new product ${productName} - createDate ${createDate}`);
 
-  task.save()
+  product.save()
       .then(() => { 
-        console.log(`Added new task ${taskName} - createDate ${createDate}`)        
+        console.log(`Added new product ${productName} - createDate ${createDate}`);        
         res.redirect('/'); })
       .catch((err) => {
           console.log(err);
@@ -40,28 +40,11 @@ router.post('/addTask', function(req, res, next) {
       });
 });
 
-router.post('/completeTask', function(req, res, next) {
-  console.log("I am in the PUT method")
-  const taskId = req.body._id;
-  const completedDate = Date.now();
-
-  Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now()})
+router.post('/deleteProduct', function(req, res, next) {
+  const productId = req.body._id;
+  Product.findByIdAndDelete(productId)
     .then(() => { 
-      console.log(`Completed task ${taskId}`)
-      res.redirect('/'); }  )
-    .catch((err) => {
-      console.log(err);
-      res.send('Sorry! Something went wrong.');
-    });
-});
-
-
-router.post('/deleteTask', function(req, res, next) {
-  const taskId = req.body._id;
-  const completedDate = Date.now();
-  Task.findByIdAndDelete(taskId)
-    .then(() => { 
-      console.log(`Deleted task $(taskId)`)      
+      console.log(`Deleted product ${productId}`);      
       res.redirect('/'); }  )
     .catch((err) => {
       console.log(err);
